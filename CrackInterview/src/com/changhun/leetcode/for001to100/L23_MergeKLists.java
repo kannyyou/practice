eb6525d3b777f67e564356742eed4b4d51550721
changhun <changhun@netseer.com>
2018-09-11 23:45:56 -0700

    pint

--- CrackInterview/src/com/changhun/leetcode/for001to100/L23_MergeKLists.java
+++ CrackInterview/src/com/changhun/leetcode/for001to100/L23_MergeKLists.java
@@ -1,63 +1,103 @@
 package com.changhun.leetcode.for001to100;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
 /**
-Merge two sorted linked lists and return it as a new list.
- The new list should be made by splicing together the nodes of the first two lists.
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
 
 Example:
 
-Input: 1->2->4, 1->3->4
-Output: 1->1->2->3->4->4
+Input:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+Output: 1->1->2->3->4->4->5->6
+
+Hard
+
+pinterest
 
  * @author changhun
  *
  */
 public class L23_MergeKLists {
 	
-	public class ListNode {
+	public static class ListNode {
 		int val;
 		ListNode next;
 		ListNode(int x) { val = x; }
 	}
-	 
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) {
-            return l2;
-        }
-        else if (l2 == null) {
-            return l1;
-        }
-        else if (l1.val < l2.val) {
-            l1.next = mergeTwoLists(l1.next, l2);
-            return l1;
-        }
-        else {
-            l2.next = mergeTwoLists(l1, l2.next);
-            return l2;
-        }
+	
+	public static void print(ListNode node) {
+		while (node != null) {
+			System.out.print(node.val); 
+			if (node.next != null)
+				System.out.print(" -> ");
+			node = node.next;
+		}
+		System.out.println();
+	}
+	
+	public static class ListNodeComparator implements Comparator<ListNode> {
+		@Override
+		public int compare(ListNode o1, ListNode o2) {
+          if (o1.val<o2.val)
+        	  return -1;
+          else if (o1.val==o2.val)
+        	  return 0;
+          else 
+        	  return 1;
+		}		
+	}
+	
+	public static class Solution {
+	    public static ListNode mergeKLists(List<ListNode> lists) {
+	        if (lists==null || lists.size()==0) return null;
+	        PriorityQueue<ListNode> queue= new PriorityQueue<ListNode>(lists.size(), new ListNodeComparator());
+	        
+	        ListNode dummy = new ListNode(0);
+	        ListNode tail=dummy;
+	        
+	        for (ListNode node:lists)
+	            if (node!=null)
+	                queue.add(node);
+	        
+	        while (!queue.isEmpty()) {
+	            tail.next=queue.poll();
+	            tail=tail.next;
+	            
+	            if (tail.next!=null)
+	                queue.add(tail.next);
+	        }
+	        return dummy.next;
+	    }
 
-    }
-    
-    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
-        // maintain an unchanging reference to node ahead of the return node.
-        ListNode prehead = new ListNode(-1);
+	}
+	
+	public static void main(String[] args) {
+		ListNode l1 = new ListNode(1);
+		l1.next = new ListNode(4);
+		l1.next.next = new ListNode(5);
+		
+		ListNode l2 = new ListNode(1);
+		l2.next = new ListNode(3);
+		l2.next.next = new ListNode(4);
+		
+		ListNode l3 = new ListNode(2);
+		l3.next = new ListNode(6);
+		
+		List<ListNode> list = new ArrayList<ListNode>();
+		list.add(l1);
+		list.add(l2);
+		list.add(l3);
+						
+//		print(Solution1.mergeTwoLists(l1, l2));
+		print(Solution.mergeKLists(list));
 
-        ListNode prev = prehead;
-        while (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                prev.next = l1;
-                l1 = l1.next;
-            } else {
-                prev.next = l2;
-                l2 = l2.next;
-            }
-            prev = prev.next;
-        }
-
-        // exactly one of l1 and l2 can be non-null at this point, so connect
-        // the non-null list to the end of the merged list.
-        prev.next = l1 == null ? l2 : l1;
-
-        return prehead.next;
-    }
+	}
 }
